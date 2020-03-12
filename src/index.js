85ef5c853d22759e18361249a2ee9f2d7d9f0321
Viktor Shelepen <viktor.sh@voicenter.com>
2020-03-12 15:34:31 +0200

    V0.0.4 published.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const { extractValue, parse, parseCommand } = require('./parser');
 const { Manager } = require('./manager')
+const { runCLI } = require('./cli')
 
 module.exports = {
+  runCLI,
   extractValue,
   parse,
   parseCommand,
